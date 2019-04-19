49fb9fa1b752c51c56a021e83b12b9083f4c4d00
faisalshafi39 <shafi.faisal@bqe.com>
2019-04-19 12:12:27 +0530

    New Update

--- docs/assets/deployment-settings.js
+++ docs/assets/deployment-settings.js
@@ -1,5 +1,5 @@
 window.deployment_settings = {
-    API_BASEURL: 'http://192.168.13.84/BQECoreApiDocumentation/api',
+    API_BASEURL: 'https://api-documentation.bqecore.com/documentationapi/api/apis',
     BASE_HREF: '/',
     IS_SINGLE_PAGE: true
 };
